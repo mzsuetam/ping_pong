f50d563e5c4ecdd8d461dd65d1279ca001e97022
Mateusz Mazur <mazurm@student.agh.edu.pl>
2021-11-16 01:59:04 +0100

    pvp gameplay ready

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame as pg
 pg.font.init()
 import os
 from random import randint as rd
+import time
 
 D_WIDTH, D_HEIGHT = 1200, 800
 WIN = pg.display.set_mode((D_WIDTH, D_HEIGHT)) 
@@ -16,6 +17,7 @@ RED = (255,0,0)
 BLUE = (0,0,255)
 
 POINTS_FONT=pg.font.SysFont('Arial', 100)
+MESSAGE_FONT=pg.font.SysFont('Arial', 120)
 BALL_IMG=pg.image.load(os.path.join('resources','ball.png'))
 BALL_IMG=pg.transform.scale(BALL_IMG,(25,25))
 
@@ -23,15 +25,16 @@ MAX_POINTS=5
 VEL=5
 BALL_VEL=5
 ball_prev_pos=[D_WIDTH/2-12.5,D_HEIGHT/2-12.5]
+ball_dir=rd(5,11)/10
 
 
-def draw_window(pl_A, pl_B, ball, A_points, B_points):
+def draw_window(pl_A, pl_B, ball, points, message):
 	#WIN.fill(WHITE)
 
 	pg.draw.rect(WIN, BLACK, BACKGROUND)
 	
-	A_points_text=POINTS_FONT.render(str(A_points), 1, GREY)
-	B_points_text=POINTS_FONT.render(str(B_points), 1, GREY)
+	A_points_text=POINTS_FONT.render(str(points[0]), 1, GREY)
+	B_points_text=POINTS_FONT.render(str(points[1]), 1, GREY)
 	WIN.blit(A_points_text, (D_WIDTH//4-A_points_text.get_width()//2,D_HEIGHT//2-A_points_text.get_height()//2))
 	WIN.blit(B_points_text, (3*D_WIDTH//4-B_points_text.get_width()//2,D_HEIGHT//2-B_points_text.get_height()//2))
 
@@ -40,7 +43,17 @@ def draw_window(pl_A, pl_B, ball, A_points, B_points):
 
 	WIN.blit(BALL_IMG, (ball.x, ball.y))
 
+	message_text=MESSAGE_FONT.render(message,1,WHITE)
+	WIN.blit(message_text,(D_WIDTH//2-message_text.get_width()//2,D_HEIGHT//2-message_text.get_height()//2)
+	)
+
 	pg.display.update() 
+
+def play_init(ball):
+	ball_dir=rd(5,11)/10
+	ball.x, ball.y = D_WIDTH//2-12.5, D_HEIGHT//2-12.5
+	ball_prev_pos[0]=ball.x+rd(1,3)-1.5
+	ball_prev_pos[1]=ball.y+rd(1,3)-1.5
 
 def handle_movement_A(keys_pressed, pl_A):
 	if keys_pressed[pg.K_w] and pl_A.y>0:
@@ -56,6 +69,7 @@ def handle_movement_B(keys_pressed, pl_B):
 
 def handle_movement_ball(ball, pl_A, pl_B):
 	D=[ball.x-ball_prev_pos[0],ball.y-ball_prev_pos[1]]
+
 	if ball.y>0 and ball.y<D_HEIGHT-ball.height:
 		if D[1]>0:
 			ball_prev_pos[1]=ball.y
@@ -68,26 +82,51 @@ def handle_movement_ball(ball, pl_A, pl_B):
 		ball_prev_pos[1]=ball.y
 		ball.y=a
 
+	if D[0]>0:
+		ball_prev_pos[0]=ball.x
+		ball.x+=BALL_VEL*ball_dir
+	else:
+		ball_prev_pos[0]=ball.x
+		ball.x-=BALL_VEL*ball_dir
+
+	if (ball.colliderect(pl_A) and ball.x>pl_A.x) or (ball.colliderect(pl_B) and ball.x<pl_B.x):
+		a=ball_prev_pos[0]
+		ball_prev_pos[0]=ball.x
+		ball.x=a
+
+def handle_score(pl_A, pl_B, ball, points):
+	if ball.x<0 or ball.x>D_WIDTH-ball.width:
+		mess="XD"
+		if ball.x<0:
+			#B scorred
+			points[1]+=1
+			mess="BLUE SCORED!"
+		if ball.x>D_WIDTH-ball.width:
+			#A scored
+			points[0]+=1
+			mess="RED SCORED!"
+		draw_window(pl_A, pl_B, ball, points, mess)
+		time.sleep(3)
+		play_init(ball)
+		
+
 def main():
 
 	game_clk=pg.time.Clock()
 
-	pl_A = pg.Rect(50,D_HEIGHT//2-D_HEIGHT//20/2 ,25,D_HEIGHT//20)
-	pl_B = pg.Rect(D_WIDTH-50-25,D_HEIGHT//2-D_HEIGHT//20/2 ,25,D_HEIGHT//20) 
+	pl_A = pg.Rect(50,D_HEIGHT//2-D_HEIGHT//20/2 ,5,D_HEIGHT//10)
+	pl_B = pg.Rect(D_WIDTH-50-25,D_HEIGHT//2-D_HEIGHT//20/2 ,5,D_HEIGHT//10) 
 
-	ball = pg.Rect(D_WIDTH//2-12.5, D_HEIGHT//2-12.5,25,25)
-	if rd(0,1):
-		ball_prev_pos[0]=-rd(1,10)/10
-	else:
-		ball_prev_pos[0]=rd(1,10)/10
-	ball_prev_pos[1]=ball.y+rd(1,3)-1.5
+	ball = pg.Rect(0,0,25,25)
+	play_init(ball)
 
-	A_points, B_points = 0,0
+	points=[0,0]
+
+	print(ball_prev_pos[0]-ball.x)
 
 	game_run = True
 	while game_run: 
 		game_clk.tick(FPS)
-
 		for event in pg.event.get():
 			#WINDOW_EXIT
 			if event.type == pg.QUIT:
@@ -101,19 +140,21 @@ def main():
 
 		handle_movement_ball(ball, pl_A, pl_B)
 
-		#win_handling
-		winner_text=""
-		if A_points >=MAX_POINTS:
-			winner_text="pl_A 1 Wins!"
-		if B_points >=MAX_POINTS:
-			winner_text="pl_A 2 Wins!"
-		# if winner_text!="":
-		# 	draw_window(A_points, B_points)
-		# 	break
+		handle_score(pl_A, pl_B, ball, points)
 
+		if points[0]>=MAX_POINTS or points[1]>=MAX_POINTS:
+			mess=""
+			if points[0] >=MAX_POINTS:
+				mess="RED Wins!"
+			if points[1] >=MAX_POINTS:
+				mess="BLUE WINS!"
+			draw_window(pl_A, pl_B, ball, points, mess)
+			time.sleep(5)
+			break
+			
 
 		#UPDATING_WINDOW
-		draw_window(pl_A, pl_B, ball, A_points, B_points)
+		draw_window(pl_A, pl_B, ball, points, "")
 
 	main()
 
